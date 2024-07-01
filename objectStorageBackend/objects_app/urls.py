522f63fd66ce1a90e7cabc90b415bc39c11c4d05
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2024-07-01 03:52:58 +0330

    update main

--- objectStorageBackend/objects_app/urls.py
+++ objectStorageBackend/objects_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import upload_file_view
+from .views import upload_file_view, download_file_view, objects_list_view
 
 urlpatterns = [
     path('upload_file', upload_file_view, name='upload-file'),
+    path('download_file', download_file_view, name='download-file'),
+    path('objects_list', objects_list_view, name='objects-list'),
 ]
--- objectStorageBackend/objects_app/views.py
+++ objectStorageBackend/objects_app/views.py
@@ -2,12 +2,11 @@ import json
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
-from .models import Object
+from .models import Object, CustomUser
 from .serializers import ObjectSerializer
 from django.http import JsonResponse
 from django.conf import settings
-from objects_app.utils import upload_file
-
+from objects_app.utils import upload_file, objects_list, download_file
 
 
 # class ObjectCreateView(generics.CreateAPIView):
@@ -54,49 +53,58 @@ def upload_file_view(request):
     else:
         return JsonResponse({'message': 'Invalid request method'}, status=405)
 
-# @csrf_exempt
-# def upload_file_view(request):
-#     bucket_name = 'object-storage-web-project'
-#     endpoint_url = settings.AWS_ENDPOINT_URL
-#     aws_access_key_id = settings.AWS_ACCESS_KEY_ID
-#     aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
-#     print("sasaass")
-#
-#     if request.method == 'POST':
-#         file = request.FILES['file']
-#         file_name = file.name
-#         file_size = file.size
-#         print(file_name)
-#
-#         object = Object.objects.create(file_name=file_name, size=file_size, type="txt", owner=settings.LOGGED_IN_USER)
-#
-#     success = upload_file(bucket_name, endpoint_url, aws_access_key_id, aws_secret_access_key, file, object.id)
-#
-#     if success:
-#         return JsonResponse({'message': 'File uploaded successfully'}, status=200)
-#     else:
-#         return JsonResponse({'message': 'Failed to upload file'}, status=500)
+
+@csrf_exempt
+def download_file_view(request):
+    bucket_name = 'object-storage-web-project'
+    endpoint_url = settings.AWS_ENDPOINT_URL
+    aws_access_key_id = settings.AWS_ACCESS_KEY_ID
+    aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+
+    if request.method == 'POST':
+        file = request.body
+        file_name = file["file_name"]
+        object_id = file["object_id"]
+        file_format = file["type"]
+        download_path = f"{file_name}.{file_format}"
+
+        success = download_file(bucket_name, endpoint_url, aws_access_key_id, aws_secret_access_key, download_path, object_id)
+
+        if success:
+            return JsonResponse({'message': 'File downloaded successfully'}, status=200)
+        else:
+            return JsonResponse({'message': 'Failed to upload file'}, status=500)
+
+    else:
+        return JsonResponse({'message': 'Invalid request method'}, status=405)
 
 
-# @csrf_exempt
-# def objects_list_view(request):
-#     bucket_name = 'object-storage-web-project'
-#     endpoint_url = settings.AWS_ENDPOINT_URL
-#     aws_access_key_id = settings.AWS_ACCESS_KEY_ID
-#     aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
-#
-#     if request.method == 'GET':
-#         # data = json.loads(request.body)
-#         # file = data.get('file')
-#         # file_name = data.get('file_name')
-#         # size = data.get('size')
-#         # type = data.get('type')
-#
-#         object = Object.objects.create(file_name=file_name, size=size, type=type, owner=user_logged_in)
-#
-#     success = upload_file(bucket_name, endpoint_url, aws_access_key_id, aws_secret_access_key, file, object.id)
-#
-#     if success:
-#         return JsonResponse({'message': 'List of objects showed successfully'}, status=200)
-#     else:
-#         return JsonResponse({'message': 'Failed to show list of objets'}, status=500)
+@csrf_exempt
+def objects_list_view(request):
+    bucket_name = 'object-storage-web-project'
+    endpoint_url = settings.AWS_ENDPOINT_URL
+    aws_access_key_id = settings.AWS_ACCESS_KEY_ID
+    aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+
+    if request.method == 'GET':
+        object_key = objects_list(bucket_name, endpoint_url, aws_access_key_id, aws_secret_access_key)
+        if object_key is not None:
+            # Fetch objects owned by the logged-in user
+            owned_objects = Object.objects.filter(owner=settings.LOGGED_IN_USER)
+
+            # Fetch objects accessed by the logged-in user
+            accessed_objects = CustomUser.objects.get(username=settings.LOGGED_IN_USER).accessed_objects.all()
+
+            # Combine both query sets into a single list
+            list_of_objects = list(owned_objects) + list(accessed_objects)
+
+            return JsonResponse({
+                'message': 'List of objects showed successfully',
+                'list_of_objects': list_of_objects
+            }, status=200)
+
+        else:
+            return JsonResponse({'message': 'Failed to show list of objets'}, status=500)
+    else:
+        return JsonResponse({'error': 'GET method required'}, status=400)
+
